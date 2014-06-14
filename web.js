6bb110fb03d0b6b2d9da4e629f2de7c55d7978b8
nucular <nucular@users.noreply.github.com>
2014-06-14 16:52:52 +0200

    Added paste deletion and fixed ID finding

--- web.js
+++ web.js
@@ -8,11 +8,12 @@ var mime = require("mime");
 
 
 // Locals
-var pastes = {}; // maps paste ID => creation time
+var pastes = {}; // maps paste ID => minutes since creation
 
 
 // Clean up pastes
 fs.readdirSync(path.join(__dirname, "pastes")).forEach(function(file) {
+  console.log("Deleting paste '" + file + "' (Paste clearing)");
   fs.unlinkSync(path.join(__dirname, "pastes", file));
 });
 
@@ -52,7 +53,8 @@ app.get("/public/:file", function(req, res) {
 
 
 // Serves a paste
-app.get(/^\/([a-z0-9]{1,2})(\.\w+)?\/?$/, function(req, res) {
+var re = new RegExp("^\\/([a-z0-9]{1," + (process.env.IDLENGTH || "2") + "})(\\.\\w+)?\\/?$");
+app.get(re, function(req, res) {
   var file = req.params[0];
   var ext = req.params[1];
 
@@ -77,26 +79,33 @@ app.post("/post", function(req, res) {
   var data = req.body.data;
   var id;
 
-  // find the first free ID until zz
-  for (var i = 0; i < 1295; i++) {
-    id = i.toString(36);
-    if (!pastes.hasOwnProperty(id))
+  // find the first free ID
+  for (var i = 0; i < Math.pow(36, process.env.IDLENGTH || 2); i++) {
+    var nid = i.toString(36);
+    if (!pastes.hasOwnProperty(nid) || pastes[nid] === null) {
+      id = nid;
       break;
+    }
   }
 
   if (!id) {
+    console.log("Paste limit reached!");
     res.redirect("/public/error.html");
+    return;
   }
 
   var file = path.join(__dirname, "pastes",  id);
 
   // Write to a file
+  console.log("Added paste '" + id + "'");
   fs.writeFile(file, data, "utf8", function(err) {
     if (err) {
+      console.log("Failed writing paste '" + id + "': " + err);
       res.redirect("/public/error.html");
     }
     else {
-      pastes[id] = new Date();
+      pastes[id] = 0;
+      console.log("Succeeded writing paste '" + id + "'");
       res.redirect("/" + id + ".txt");
     }
   });
@@ -107,6 +116,26 @@ app.post("/post", function(req, res) {
 app.get("*", function(req, res) {
   res.status(404).sendfile(path.join(__dirname, "public", "404.html"));
 });
+
+
+// Delete too old pastes every minute
+setInterval(function() {
+  for (var k in pastes) {
+    if (pastes.hasOwnProperty(k)) {
+      if (pastes[k] >= (process.env.MAXAGE || 10)) {
+        console.log("Deleting paste '" + k + "' (" + pastes[k] + " minutes old)");
+        pastes[k] = null;
+        fs.unlink(path.join(__dirname, "pastes", k), function(err) {
+          if (err)
+            console.log("Failed deleting paste: " + err);
+        });
+      }
+      else if (pastes[k] !== null) {
+        pastes[k]++;
+      }
+    }
+  }
+}, 1000 * 60);
 
 
 var port = Number(process.env.PORT || 5000);
